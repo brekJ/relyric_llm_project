5368a526a87e20b4a2e4f0ea9db350f442f20cca
connet <jjh5720@gmail.com>
2024-08-26 14:48:07 +0900

    feat: main crawler 파일에 gpt 감성분석 코드 및 mongoDB 적재 코드 추가

--- crawling/lyric_crawler.py
+++ crawling/lyric_crawler.py
@@ -6,11 +6,32 @@ from selenium.webdriver.common.by import By
 
 import time
 import random
+import gc
 
+from chat_driver import ChatDriver as ChatDriver
+
+from pymongo import MongoClient
+
+def xpath_element(driver, xpath):
+    return driver.find_element(By.XPATH, xpath)
+
+# MongoDB 클라이언트 생성
+client = MongoClient('localhost', 27017)
+
+# 데이터베이스 선택
+db = client['mel_lyrics']
+
+# 컬렉션 선택
+collection = db['songs']
+
+# Sleep 시간 설정
 SLEEP_TIMES = [x/10 for x in [40, 60, 10, 50]]
 
 # webdriver_manager를 사용하여 크롬 드라이버의 실행 경로 설정
 service = Service(executable_path=ChromeDriverManager().install())
+
+# chatgpt를 사용할 driver 생성
+driver_chat = ChatDriver()
 
 # 설정된 service를 사용하여 크롬 드라이버 인스턴스 생성
 driver = ChromeDriver(service=service)
@@ -18,61 +39,98 @@ driver.implicitly_wait(10)  # 최대 10초 동안 대기
 
 # 테스트하려는 웹사이트 열기
 driver.get("https://www.melon.com/chart/index.htm")
-x_path = r'/html/body/div/div[2]/div/div[2]/ul[1]/li[1]/div/div/button'
-driver.find_element(By.XPATH, x_path).click()
 
-x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/h4[2]/a'
-driver.find_element(By.XPATH, x_path).click()
+# 차트 파인더 페이지로 이동
+xpath_element(driver, r'/html/body/div/div[2]/div/div[2]/ul[1]/li[1]/div/div/button').click()
+
+# 월간 차트 클릭
+xpath_element(driver, r'/html/body/div/div[3]/div/div/form/div[1]/div/h4[2]/a').click()
+
+# 중복 검사할 리스트 생성
+check_list = []
+
+# 일련번호 정의
+id = 0
+
 try:
+    # 시대별 차트 클릭
     for period in range(5):
         period_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[1]/div[1]/ul/li[' + str(period + 1) + r']/span'
-        period_btn = driver.find_element(By.XPATH, period_x_path)
+        period_btn = xpath_element(driver, period_x_path)
         period_btn.click()
         time.sleep(random.choice(SLEEP_TIMES))
+        # 년도별 차트 클릭
         for year in range(10):
             if period == 0 and year >= 5:
                 continue
             if period == 4 and year >= 6:
                 continue
             year_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[2]/div[1]/ul/li[' + str(year + 1) + r']/span'
-            year_btn = driver.find_element(By.XPATH, year_x_path)
+            year_btn = xpath_element(driver, year_x_path)
             year_btn.click()
             time.sleep(random.choice(SLEEP_TIMES))
+            # 월별 차트 클릭
             for month in range(12):
                 if period == 0 and year == 0 and month >= 7:
                     continue
                 if period == 4 and year == 5 and month >= 11:
                     continue
-                month_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[3]/div[1]/ul/li'
-                month_btns = driver.find_elements(By.XPATH, month_x_path)
-                for month_btn in month_btns:
-                    month_btn.find_element(By.XPATH, './span').click()
-                    time.sleep(random.choice(SLEEP_TIMES))
+                month_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[3]/div[1]/ul/li[' + str(month + 1) + r']/span'
+                driver.find_element(By.XPATH, month_x_path).click()
+                time.sleep(random.choice(SLEEP_TIMES))
+                
+                cat_btns = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[5]/div[1]/ul/li')
+                cat_btns[0].find_element(By.XPATH, './span').click()
+                time.sleep(random.choice(SLEEP_TIMES))
                     
-                    cat_btns = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[5]/div[1]/ul/li')
-                    cat_btns[0].find_element(By.XPATH, './span').click()
+                btn_x_path = r'/html/body/div/div[3]/div/div/form/div[2]/button'
+                xpath_element(driver, btn_x_path).click()
+                time.sleep(random.choice(SLEEP_TIMES))
+                
+                elements = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/div/div[1]/div[2]/form/div[1]/table/tbody/tr')
+                for el_idx, element in enumerate(elements):
+                    element.find_element(By.XPATH, './td[4]/div/a').click()
                     time.sleep(random.choice(SLEEP_TIMES))
-                    
-                    btn_x_path = r'/html/body/div/div[3]/div/div/form/div[2]/button'
-                    driver.find_element(By.XPATH, btn_x_path).click()
-                    time.sleep(random.choice(SLEEP_TIMES))
-                    
-                    elements = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/div/div[1]/div[2]/form/div[1]/table/tbody/tr')
-                    for element in elements:
-                        element.find_element(By.XPATH, './td[4]/div/a').click()
-                        time.sleep(random.choice(SLEEP_TIMES))
-                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[1]').text)
-                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[2]').text)
-                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[2]/dl/dd[3]').text)
-                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/div[2]/div[2]/div').text)
+            
+                    song_title = driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[1]').text
+                    song_singer = driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[2]').text
+                    song_gengre = driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[2]/dl/dd[3]').text
+                    song_lyric = driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/div[2]/div[2]/div').text
+                    song_mood = driver_chat.make_mood(song_lyric)
+                    if song_mood is None:
+                        assert False, "Failed to retrieve the mood"
+                    # check_list에 중복 검사
+                    if (song_title, song_singer) in check_list:
                         driver.back()
                         time.sleep(random.choice(SLEEP_TIMES))
-                    driver.quit()
-                    exit()
-except:
+                        continue
+                        
+                    # mongoDB에 저장할 document 생성
+                    document = {
+                        'id':id,
+                        'title':song_title,
+                        'singer':song_singer,
+                        'gengre':song_gengre,
+                        'lyric':song_lyric,
+                        'mood':song_mood}
+                    # document 삽입
+                    collection.insert_one(document)
+                    
+                    check_list.append((song_title, song_singer))
+                    driver.back()
+                    print(f"{song_title, song_singer}")
+                    print(f"{check_list=}")
+                    gc.collect()
+                    time.sleep(random.choice(SLEEP_TIMES))
+                    id += 1
+except Exception as e:
+    print(f"Error encountered: {e}")
     print(f"period : {period + 1}")
     print(f"year : {year + 1}")
     print(f"month : {month + 1}")
+    print(f"el_idx : {el_idx + 1}")
+    print(f"title : {song_title}")
+    print(f"singer : {song_singer}")
             
         
         
@@ -80,28 +138,3 @@ except:
 # 필요한 작업 수행 후 드라이버 종료
 driver.quit()
 exit()
-r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[1]/div[1]/ul/li[2]/span/input'
-for i in range(7):
-    year_month_btn.click()
-    month_btn = r'#conts > div.calendar_prid > div > div > dl > dd.month_calendar > ul > li:nth-child(' + str(i+1) + r') > a'
-    driver.find_element(By.CSS_SELECTOR, month_btn).click()
-    time.sleep(random.choice(SLEEP_TIMES))
-
-for index in range(100, 800+1, 100):
-    url = f"https://www.melon.com/chart/month/index.htm?classCd=GN0{index}"
-    driver.get(url)
-    xpath = r'//*[@id="lst50"]/td[5]/div/a'
-    elements = driver.find_elements(By.XPATH, xpath)
-    for element in elements:
-        element.click()
-        spread = driver.find_element(By.XPATH, r'//*[@id="lyricArea"]/button/i')
-        spread.click()
-        title = driver.find_element(By.XPATH, r'//*[@id="downloadfrm"]/div/div/div[2]/div[1]/div[1]').text
-        lyric = driver.find_element(By.XPATH, r'//*[@id="d_video_summary"]').text
-        with open(f"../data/{title}.txt", "w") as f:
-            f.write(lyric)
-        driver.back()
-        time.sleep(random.choice(SLEEP_TIMES))
-    time.sleep(random.choice(SLEEP_TIMES))
-
-
