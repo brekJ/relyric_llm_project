6dfa4824b6e13968fa542627f01e82297b9630f8
brekJ <jjh5720@gmail.com>
2024-08-26 09:25:05 +0900

    feat:crawler에 차트 데이터 크롤링 코드 추가

--- crawling/lyric_crawler.py
+++ crawling/lyric_crawler.py
@@ -17,6 +17,74 @@ driver.implicitly_wait(10)  # 최대 10초 동안 대기
 
 # 테스트하려는 웹사이트 열기
 driver.get("https://www.melon.com/chart/index.htm")
+x_path = r'/html/body/div/div[2]/div/div[2]/ul[1]/li[1]/div/div/button'
+driver.find_element(By.XPATH, x_path).click()
+
+x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/h4[2]/a'
+driver.find_element(By.XPATH, x_path).click()
+try:
+    for period in range(5):
+        period_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[1]/div[1]/ul/li[' + str(period + 1) + r']/span'
+        period_btn = driver.find_element(By.XPATH, period_x_path)
+        period_btn.click()
+        time.sleep(random.choice(SLEEP_TIMES))
+        for year in range(10):
+            if period == 0 and year >= 5:
+                continue
+            if period == 4 and year >= 6:
+                continue
+            year_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[2]/div[1]/ul/li[' + str(year + 1) + r']/span'
+            year_btn = driver.find_element(By.XPATH, year_x_path)
+            year_btn.click()
+            time.sleep(random.choice(SLEEP_TIMES))
+            for month in range(12):
+                if period == 0 and year == 0 and month >= 7:
+                    continue
+                if period == 4 and year == 5 and month >= 11:
+                    continue
+                month_x_path = r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[3]/div[1]/ul/li'
+                month_btns = driver.find_elements(By.XPATH, month_x_path)
+                for month_btn in month_btns:
+                    month_btn.find_element(By.XPATH, './span').click()
+                    time.sleep(random.choice(SLEEP_TIMES))
+                    
+                    cat_btns = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[5]/div[1]/ul/li')
+                    cat_btns[0].find_element(By.XPATH, './span').click()
+                    time.sleep(random.choice(SLEEP_TIMES))
+                    
+                    btn_x_path = r'/html/body/div/div[3]/div/div/form/div[2]/button'
+                    driver.find_element(By.XPATH, btn_x_path).click()
+                    time.sleep(random.choice(SLEEP_TIMES))
+                    
+                    elements = driver.find_elements(By.XPATH, r'/html/body/div/div[3]/div/div/div/div[1]/div[2]/form/div[1]/table/tbody/tr')
+                    for element in elements:
+                        element.find_element(By.XPATH, './td[4]/div/a').click()
+                        time.sleep(random.choice(SLEEP_TIMES))
+                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[1]').text)
+                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[1]/div[2]').text)
+                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/form/div/div/div[2]/div[2]/dl/dd[3]').text)
+                        print(driver.find_element(By.XPATH, r'/html/body/div[1]/div[3]/div/div/div/div[2]/div[2]/div').text)
+                        driver.back()
+                        time.sleep(random.choice(SLEEP_TIMES))
+                    driver.quit()
+                    exit()
+except:
+    print(f"period : {period + 1}")
+    print(f"year : {year + 1}")
+    print(f"month : {month + 1}")
+            
+        
+        
+
+# 필요한 작업 수행 후 드라이버 종료
+driver.quit()
+exit()
+r'/html/body/div/div[3]/div/div/form/div[1]/div/div/div[1]/div[1]/ul/li[2]/span/input'
+for i in range(7):
+    year_month_btn.click()
+    month_btn = r'#conts > div.calendar_prid > div > div > dl > dd.month_calendar > ul > li:nth-child(' + str(i+1) + r') > a'
+    driver.find_element(By.CSS_SELECTOR, month_btn).click()
+    time.sleep(random.choice(SLEEP_TIMES))
 
 for index in range(100, 800+1, 100):
     url = f"https://www.melon.com/chart/month/index.htm?classCd=GN0{index}"
@@ -32,11 +100,5 @@ for index in range(100, 800+1, 100):
         driver.back()
         time.sleep(random.choice(SLEEP_TIMES))
     time.sleep(random.choice(SLEEP_TIMES))
-    
-    
 
 
-time.sleep(3)
-
-# 필요한 작업 수행 후 드라이버 종료
-driver.quit()
